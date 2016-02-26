45b594cb0a8905468ebbc4707ac41a17d655e105
Julia <account.general@yahoo.com>
2016-02-25 21:32:31 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv();
+var port = appEnv.port;
+var host = '0.0.0.0'
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    port,
+    host
+  });
 });
 
 module.exports = router;
